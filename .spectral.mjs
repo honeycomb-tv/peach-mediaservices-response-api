7b9eaed2839371bd8defae432684b565564fc535
Arvin Benitez <arvin.benitez@peachvideo.com>
2023-03-29 16:59:31 +0100

    Added 3 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjE5MzA1OA/spectral.js?branch_slug=master&token=31b9bda3-7709-4a7f-9038-b087d8803638";
+export default { extends: ruleset };
